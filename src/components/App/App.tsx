98f35620b367ff18333325459bf96a972b9c5a6e
Maksim <maksim.tatarinov@indels.org>
2025-10-04 22:03:44 +0900

    sprint-1/step-1/ replace app

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,6 +1,6 @@
-import AppHeader from "../components/AppHeader/ui/AppHeader";
-import BurgerConstructor from "../components/BurgerConstructor/BurgerConstructor";
-import BurgerIngredients from "../components/BurgerIngredients/BurgerIngredients";
+import AppHeader from "../AppHeader/ui/AppHeader";
+import BurgerConstructor from "../BurgerConstructor/BurgerConstructor";
+import BurgerIngredients from "../BurgerIngredients/BurgerIngredients";
 import styles from "./App.module.css";
 
 function App() {
