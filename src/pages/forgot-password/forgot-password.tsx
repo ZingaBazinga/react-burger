c94fc129407faeb31d9d0ac77f503ded6abf3c3d
ZingaBazinga <maksimtatarinov27@gmail.com>
2025-11-02 13:10:43 +0900

    sprint-3/step-1/ верстка страницы ввостановления пароля

--- src/pages/forgot-password/forgot-password.tsx
+++ src/pages/forgot-password/forgot-password.tsx
@@ -1,3 +1,32 @@
+import { useState } from "react";
+import styles from "./forgot-password.module.css";
+import { Button, Input } from "@ya.praktikum/react-developer-burger-ui-components";
+
 export function ForgotPassword() {
-    return <>forgot-password</>;
+    const [email, setEmail] = useState("");
+    return (
+        <div className={styles.container}>
+            <h1 className="text text_type_main-medium">Восстановление пароля</h1>
+            <Input
+                type="text"
+                placeholder="Укажите e-mail"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                size="default"
+                onPointerEnterCapture={() => {}}
+                onPointerLeaveCapture={() => {}}
+            />
+            <div className={styles.buttons}>
+                <Button htmlType="button" type="primary" size="medium">
+                    Восстановить
+                </Button>
+                <p className={`text text_type_main-default text_color_inactive ${styles.description}`}>
+                    Вспомнили пароль?{" "}
+                    <Button htmlType="button" type="secondary" size="medium" extraClass={styles.link}>
+                        Войти
+                    </Button>
+                </p>
+            </div>
+        </div>
+    );
 }
