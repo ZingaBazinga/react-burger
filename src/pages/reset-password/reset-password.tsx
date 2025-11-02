7918240522b65b281b1ed4bd411f05e63ab1c240
ZingaBazinga <maksimtatarinov27@gmail.com>
2025-11-02 13:14:52 +0900

    sprint-3/step-1/ верстка страницы ввостановления пароля новый пароль

--- src/pages/reset-password/reset-password.tsx
+++ src/pages/reset-password/reset-password.tsx
@@ -1,3 +1,45 @@
+import { useState } from "react";
+import styles from "./reset-password.module.css";
+import { Button, Input } from "@ya.praktikum/react-developer-burger-ui-components";
+
 export function ResetPassword() {
-    return <>reset-password</>;
+    const [password, setPassword] = useState("");
+    const [showPassword, setShowPassword] = useState(false);
+    const [code, setCode] = useState("");
+    return (
+        <div className={styles.container}>
+            <h1 className="text text_type_main-medium">Восстановление пароля</h1>
+            <Input
+                type={showPassword ? "text" : "password"}
+                placeholder="Введите новый пароль"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                size="default"
+                icon={showPassword ? "ShowIcon" : "HideIcon"}
+                onIconClick={() => setShowPassword(!showPassword)}
+                onPointerEnterCapture={() => {}}
+                onPointerLeaveCapture={() => {}}
+            />
+            <Input
+                type="text"
+                placeholder="Введите код из письма"
+                value={code}
+                onChange={(e) => setCode(e.target.value)}
+                size="default"
+                onPointerEnterCapture={() => {}}
+                onPointerLeaveCapture={() => {}}
+            />
+            <div className={styles.buttons}>
+                <Button htmlType="button" type="primary" size="medium">
+                    Сохранить
+                </Button>
+                <p className={`text text_type_main-default text_color_inactive ${styles.description}`}>
+                    Вспомнили пароль?{" "}
+                    <Button htmlType="button" type="secondary" size="medium" extraClass={styles.link}>
+                        Войти
+                    </Button>
+                </p>
+            </div>
+        </div>
+    );
 }
